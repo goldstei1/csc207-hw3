dd0088141be904ddf59be59c4a4080ecdfc776b5
Daniel Goldstein <dpgold94@gmail.com>
2013-09-13 14:08:33 -0500

    Fixed part E and commented it. Added more tests.

--- src/edu/grinnell/csc207/goldstei1/utils/Calculator.java
+++ src/edu/grinnell/csc207/goldstei1/utils/Calculator.java
@@ -5,58 +5,65 @@ import java.math.BigInteger;
 public class Calculator {
     
     
-    public static BigInteger eval0(String expression) {
-	
-	StringBuffer evaluator = new StringBuffer(expression);
-	int i = 0;
-	char nextOperation = ' ';
-	int begOfNum;
-	BigInteger endValue = new BigInteger("0");
-	
-	
-	while(i < evaluator.length() && evaluator.charAt(i) != ' ') {
-	    i++;
-	}
-	endValue = endValue.add(new BigInteger(evaluator.substring(0, i).toString()));
-	i++;
-	
-	while (i < evaluator.length()) {
-	    switch (evaluator.charAt(i)) {
-	    case '+': nextOperation = '+';
-	    break;
-	    case '-': nextOperation = '-';
-	    break;
-	    case '*': nextOperation = '*';
-	    break;
-	    case '/': nextOperation = '/';
-	    break;
-	    case '^': nextOperation = '^';
-	    }
-	    i += 2;
-	    
-	    begOfNum = i;
-	    
-	    while(i < evaluator.length() && i != ' ') {
-		i++;
-	    }
-	    
-	    switch (nextOperation) {
-	    case '+': endValue = endValue.add(new BigInteger(evaluator.substring(begOfNum, i).toString()));
-	    break;
-	    case '-': endValue = endValue.subtract(new BigInteger(evaluator.substring(begOfNum, i).toString()));
-	    break;
-	    case '*': endValue = endValue.multiply(new BigInteger(evaluator.substring(begOfNum, i).toString()));
-	    break;
-	    case '/': endValue = endValue.divide(new BigInteger(evaluator.substring(begOfNum, i).toString()));
-	    break;
-	    case '^': endValue = endValue.pow(new Integer(evaluator.substring(begOfNum, i).toString()));
-	    break;
-	    }
-	    i +=2;
-	}
-	
-	return endValue;
-	
-    }
+	/**
+	 * Method to solve a mathematical expression given as a string
+	 *  with spaces between each number and operation. The numbers
+	 *  in the string can be larger than a normal integer.
+	 */
+	public static BigInteger eval0(String expression) {
 
+		StringBuffer evaluator = new StringBuffer(expression);
+		BigInteger endValue = new BigInteger("0"); //first number in string is added later
+		char nextOperation = '+'; // first operation is always add
+		int begOfNum;
+		int i = 0; //starting index of the evaluator string
+
+
+		while (i < evaluator.length()) {
+			//Check for which operation will happen next
+			// first time through will skip this section
+			switch (evaluator.charAt(i)) {
+			case '+': nextOperation = '+';
+			i += 2;
+			break;
+			case '-': nextOperation = '-';
+			i += 2;
+			break;
+			case '*': nextOperation = '*';
+			i += 2;
+			break;
+			case '/': nextOperation = '/';
+			i += 2;
+			break;
+			case '^': nextOperation = '^';
+			i += 2;
+			break;
+			}//switch
+
+			//index of the beginning of the next number
+			begOfNum = i;
+
+			while(i < evaluator.length() && evaluator.charAt(i) != ' ') {
+				i++;
+			}//while
+
+			//end of next number is now i so do the operation
+			switch (nextOperation) {
+			case '+': endValue = endValue.add(new BigInteger(evaluator.substring(begOfNum, i).toString()));
+			break;
+			case '-': endValue = endValue.subtract(new BigInteger(evaluator.substring(begOfNum, i).toString()));
+			break;
+			case '*': endValue = endValue.multiply(new BigInteger(evaluator.substring(begOfNum, i).toString()));
+			break;
+			case '/': endValue = endValue.divide(new BigInteger(evaluator.substring(begOfNum, i).toString()));
+			break;
+			case '^': endValue = endValue.pow(new Integer(evaluator.substring(begOfNum, i).toString()));
+			break;
+			}//switch
+
+			i++;
+		}//While
+
+		return endValue;
+	}
 }
--- src/edu/grinnell/csc207/goldstei1/utils/CalculatorTest.java
+++ src/edu/grinnell/csc207/goldstei1/utils/CalculatorTest.java
@@ -14,6 +14,9 @@ public class CalculatorTest {
 	assertEquals(new BigInteger("2"), Calculator.eval0("1 + 1"));
 	assertEquals(new BigInteger("4"), Calculator.eval0("1 + 2 + 1"));
 	assertEquals(new BigInteger("9"), Calculator.eval0("1 + 2 * 3"));
+	assertEquals(new BigInteger("298576849383746829123456796"),
+					 Calculator.eval0("2985768493837468291234567890 / 10 + 7"));
+	assertEquals(new BigInteger("16"), Calculator.eval0("1 + 2 / 3 + 7 - 6 ^ 4"));
     }
 
 }
